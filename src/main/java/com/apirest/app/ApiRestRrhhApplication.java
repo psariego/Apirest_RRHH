c8a9a386171483271dfe0bef2320fa6b97b32a82
psariego <pablo.sariego-alonso@capgemini.com>
2022-03-03 12:48:42 +0100

    arreglos varios

--- src/main/java/com/apirest/app/ApiRestRrhhApplication.java
+++ src/main/java/com/apirest/app/ApiRestRrhhApplication.java
@@ -3,7 +3,11 @@ package com.apirest.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title ="API RRHH",version ="1.0",description ="Crud completo de Recursos Humanos"))
 public class ApiRestRrhhApplication {
 
 	public static void main(String[] args) {
